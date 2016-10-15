0d112acb56a6d08aac882506193c8f7bc032e27a
kellyselden <kellyselden@gmail.com>
2016-10-14 17:09:27 -0700

    add a fastboot acceptance test

--- addon/services/fetch.js
+++ addon/services/fetch.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
-import fetch from 'ember-network/fetch';
+import _fetch from 'ember-network/fetch';
 
 const {
   Service
 } = Ember;
+
+function isFastBoot() {
+  return typeof FastBoot !== 'undefined';
+}
+
+// node-fetch doesn't polyfill global fetch, unlike GitHub's fetch
+// in order to mock global fetch in the tests AND support FastBoot,
+// we must unify by making node-fetch global
+if (isFastBoot()) {
+  window.fetch = _fetch;
+}
 
 export default Service.extend({
   fetch() {
