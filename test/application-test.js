1545f077c3fa368869db5d760810728d713e9f1a
kellyselden <kellyselden@gmail.com>
2016-10-14 17:09:27 -0700

    add a fastboot acceptance test

---
+++ test/application-test.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect;
+var RSVP = require('rsvp');
+var request = RSVP.denodeify(require('request'));
+var AddonTestApp = require('ember-cli-addon-tests').AddonTestApp;
+
+describe('Acceptance | FastBoot', function() {
+  this.timeout(300000);
+
+  var app;
+
+  before(function() {
+    app = new AddonTestApp();
+
+    return app.create('dummy', {
+      fixturesPath: 'tests'
+    }).then(function() {
+      app.editPackageJSON(function(pkg) {
+        pkg['devDependencies']['ember-cli-fastboot'] = '1.0.0-beta.9';
+        pkg['devDependencies']['ember-network'] = '0.3.0';
+      });
+      return app.run('npm', 'install');
+    }).then(function() {
+      return app.startServer({
+        command: 'fastboot',
+        additionalArguments: ['--serve-assets']
+      });
+    });
+  });
+
+  after(function() {
+    return app.stopServer();
+  });
+
+  it('can find the fetch global', function() {
+    return request('http://localhost:49741')
+      .then(function(response) {
+        expect(response.body).to.contain('Error: only absolute urls are supported');
+      });
+  });
+});
